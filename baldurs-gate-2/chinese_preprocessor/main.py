f0a53544d13cbb5086c47b35d442b3983fcbaf0f
Matthew Delaney <mldelaney94@gmail.com>
2026-06-07 21:39:21 +1000

    add activate and setup venv scripts

--- baldurs-gate-2/chinese_preprocessor/main.py
+++ baldurs-gate-2/chinese_preprocessor/main.py
@@ -1,24 +1,48 @@
 import pinyiniser as pyer
 
-do_not_parse = {'CHARNAME', 'BROTHERSISTER', 'DAYANDMONTH',
-        'DAYNIGHT', 'DAYNIGHTALL', 'GABBER', 'GAMEDAY', 'GAMEDAYS',
-        'GIRLBOY', 'HESHE', 'HIMHER', 'HISHER', 'LADYLORD', 'LEVEL',
-        'MALEFEMALE', 'MANWOMAN', 'MONTH', 'MONTHNAME', 'DAY', 'PLAYER1-6',
-        'PRO_BROTHERSISTER', 'PRO_CLASS', 'PRO_GIRLBOY', 'PRO_HESHE',
-        'PRO_HIMHER', 'PRO_HISHER', 'PRO_LADYLORD', 'PRO_MALEFEMALE',
-        'PRO_MANWOMAN', 'PRO_MASTERMISTRESS', 'PRO_RACE', 'PRO_SIRMAAM'
-        'PRO_SONDAUGHTER', 'RACE', 'SIRMAAM', 'SONDAUGHTER', 'TM', 'YEAR',
-        'SPELLLEVEL', 'WEAPONNAME', 'SPECIALABILITYNAME', 'ITEMCOST',
-        'ITEMNAME', 'DURATION', 'HOUR' , 'PRO', 'PLAYER1', 'PLAYER2',
-        'PLAYER3', 'PLAYER4', 'PLAYER5', 'PLAYER6', 'DAMAGER', 'DAMAGEE',
-        'AMOUNT', 'TYPE', 'FIGHTERTYPE', 'MAGESCHOOL', 'RESISTED',
-        'SERVERVERSION', 'CLIENTVERSION', 'MINIMUM', 'MAXIMUM', 'script',
-        'CLASS', 'CurrentChapter', 'HP', 'EXPERIENCE', 'NEXTLEVEL',
-        'number', 'DURATIONNOAND', 'DOTS1', 'DOTS2', 'DOTS3', 'DOTS4',
-        'DOTS5', 'EXPERIENCEAMOUNT', 'TARGET', 'CREATURE', 'LEVELDIF',
-        'losing', 'battle', 'RESOURCE', 'PRO_HEHER', 'AREA_NAME',
-        'MISSING_CONTENT', 'PERCENT', 'COMPLETE', 'TIME', 'REMAINING'}
+# Infinity Engine substitution tokens as they appear in dialogue strings.
+GAME_TOKENS = {'<CHARNAME>', '<BROTHERSISTER>', '<DAYANDMONTH>',
+  '<DAYNIGHT>', '<DAYNIGHTALL>', '<GABBER>', '<GAMEDAY>', '<GAMEDAYS>',
+  '<GIRLBOY>', '<HESHE>', '<HIMHER>', '<HISHER>', '<LADYLORD>', '<LEVEL>',
+  '<MALEFEMALE>', '<MANWOMAN>', '<MONTH>', '<MONTHNAME>', '<DAY>', '<PLAYER1-6>',
+  '<PRO_BROTHERSISTER>', '<PRO_CLASS>', '<PRO_GIRLBOY>', '<PRO_HESHE>',
+  '<PRO_HIMHER>', '<PRO_HISHER>', '<PRO_LADYLORD>', '<PRO_MALEFEMALE>',
+  '<PRO_MANWOMAN>', '<PRO_MASTERMISTRESS>', '<PRO_RACE>', '<PRO_SIRMAAM>',
+  '<PRO_SONDAUGHTER>', '<RACE>', '<SIRMAAM>', '<SONDAUGHTER>', '<TM>', '<YEAR>',
+  '<SPELLLEVEL>', '<WEAPONNAME>', '<SPECIALABILITYNAME>', '<ITEMCOST>',
+  '<ITEMNAME>', '<DURATION>', '<HOUR>', '<PRO>', '<PLAYER1>', '<PLAYER2>',
+  '<PLAYER3>', '<PLAYER4>', '<PLAYER5>', '<PLAYER6>', '<DAMAGER>', '<DAMAGEE>',
+  '<AMOUNT>', '<TYPE>', '<FIGHTERTYPE>', '<MAGESCHOOL>', '<RESISTED>',
+  '<SERVERVERSION>', '<CLIENTVERSION>', '<MINIMUM>', '<MAXIMUM>', '<script>',
+  '<CLASS>', '<CurrentChapter>', '<HP>', '<EXPERIENCE>', '<NEXTLEVEL>',
+  '<number>', '<DURATIONNOAND>', '<DOTS1>', '<DOTS2>', '<DOTS3>', '<DOTS4>',
+  '<DOTS5>', '<EXPERIENCEAMOUNT>', '<TARGET>', '<CREATURE>', '<LEVELDIF>',
+  '<losing>', '<battle>', '<RESOURCE>', '<PRO_HEHER>', '<AREA_NAME>',
+  '<MISSING_CONTENT>', '<PERCENT>', '<COMPLETE>', '<TIME>', '<REMAINING>'
+}
 
-do_not_parse = do_not_parse.union(pyer.do_not_parse_set)
+SPECIAL_TOKENS = GAME_TOKENS | pyer.special_tokens
+
+def add_pinyin(strings: list[str], d, REPLACEMENTS: dict[str, str]):
+  for string in strings:
+    chinese, pinyin = pyer.get_segments_and_pinyin(string, d, SPECIAL_TOKENS)
+    chinese = '\u200B'.join(chinese)
+    pinyin = ' '.join(pinyin)
+    if (chinese.count('\n') > 1):
+      string = chinese + '\n\n' + pinyin
+    else:
+      string = chinese + '\n' + pinyin
+    for old, new in REPLACEMENTS.items():
+      string = string.replace(old, new)
+  return strings
 
 def main(lang, encoding):
+  print(f"Processing {lang} with encoding {encoding}")
+
+  tlk = read_tlk('materials/translations/dialog_zh.tlk')
+
+  tlk.map_strings(
+
+  )
+
+  return None
